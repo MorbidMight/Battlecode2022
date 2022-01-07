30965775e3f2992f9d95c0b448854209c3dfddcb
Todd <todd.mcknight06@gmail.com>
2022-01-07 16:36:54 -0500

    Miners don't sit on their ass waiting for their buddies to move

--- src/myBot/MinerAI.java
+++ src/myBot/MinerAI.java
@@ -13,7 +13,7 @@ public class MinerAI {
      */
     static void runMiner(RobotController rc) throws GameActionException {
         // Try to mine on squares around us.
-
+        //Mining
         MapLocation me = rc.getLocation();
         for (int dx = -1; dx <= 1; dx++) {
             for (int dy = -1; dy <= 1; dy++) {
@@ -23,28 +23,40 @@ public class MinerAI {
                 while (rc.canMineGold(mineLocation)) {
                     rc.mineGold(mineLocation);
                 }
-                while (rc.canMineLead(mineLocation)&&rc.senseLead(mineLocation)>1) {
+                while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > 1) {
                     rc.mineLead(mineLocation);
                 }
             }
         }
+
+
+        //movement
         Direction dir;
         MapLocation[] k = rc.senseNearbyLocationsWithLead(rc.getType().visionRadiusSquared);
-
-        // hi urav
-        int indexOfBestSquareInK = 0;
-        for(int i = 0; i<k.length;i++)
-        {
-            if(rc.senseLead(k[i])>rc.senseLead(k[indexOfBestSquareInK]))
-                indexOfBestSquareInK=i;
+        //            dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        int bestPossibleLead = 0;
+        int indexBest = -1;
+        for (int i = 0; i < k.length; i++) {
+            if (rc.senseLead(k[i]) > bestPossibleLead && !rc.isLocationOccupied(k[i])) {
+                indexBest = i;
+                bestPossibleLead = rc.senseLead(k[indexBest]);
+            }
         }
-        if(k.length == 0)
+        //Best possible lead is the amount of lead on the best square, 0 if no lead
+        //IndexBest is the index of the best square in K, -1 if no potential lead
+        if (indexBest == -1&&rc.senseLead(rc.getLocation())==0) {
             dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
-        else
-            dir = rc.getLocation().directionTo(k[indexOfBestSquareInK]);
-        if (rc.canMove(dir)) {
-            rc.move(dir);
-            System.out.println("I moved!");
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+            }
+        } else if (!k[indexBest].equals(rc.getLocation())&&(rc.senseLead(k[indexBest])>rc.senseLead(rc.getLocation()))) {
+            dir = rc.getLocation().directionTo(k[indexBest]);
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+                System.out.println("I moved!");
+            }
         }
     }
 }
+
+
