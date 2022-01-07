2ad1b0269f18489a95b636101fc937a2ced2e7a5
BntM <BinitM@protonmail.com>
2022-01-07 12:56:58 -0500

    added myBot with partially working soldier group

---
+++ src/myBot/ArchonAI.java
@@ -0,0 +1,50 @@
+package myBot;
+
+import battlecode.common.*;
+
+public class ArchonAI {
+    /**
+     * Run a single turn for an Archon.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+
+    static void runArchon(RobotController rc) throws GameActionException {
+        // Pick a direction to build in.
+        //check for a possible repair
+        Team ally = rc.getTeam();
+        Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        RobotInfo[] k = rc.senseNearbyRobots(20, ally);
+        //for loop to check which possible heal is lowest on health
+        int q = 100; // holder variable for lowest possible health to heal
+        int w = 0; // holder variable for index of lowest possible heal
+        if(k.length >= 1) {
+            for (int a = 0; a < k.length; a++) {
+                if(k[a].getHealth()< q) {
+                    q = k[a].getHealth();
+                    w = a;
+                }
+            }
+            if(q < 15 && rc.canRepair(k[w].getLocation()))
+                rc.repair(k[w].getLocation());
+        }
+
+        if(rc.getTeamLeadAmount(rc.getTeam()) >= 75)
+        {
+            if (Math.random() > 0.7) {
+                // Let's try to build a miner.
+                rc.setIndicatorString("Trying to build a miner");
+                if (rc.canBuildRobot(RobotType.MINER, dir)) {
+                    rc.buildRobot(RobotType.MINER, dir);
+
+                }
+            } else {
+                // Let's try to build a soldier.
+                rc.setIndicatorString("Trying to build a soldier");
+                if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
+                    rc.buildRobot(RobotType.SOLDIER, dir);
+
+                }
+            }
+        }
+    }
+}
---
+++ src/myBot/MinerAI.java
@@ -0,0 +1,50 @@
+package myBot;
+
+import battlecode.common.Direction;
+import battlecode.common.GameActionException;
+import battlecode.common.MapLocation;
+import battlecode.common.RobotController;
+
+public class MinerAI {
+
+    /**
+     * Run a single turn for a Miner.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    static void runMiner(RobotController rc) throws GameActionException {
+        // Try to mine on squares around us.
+
+        MapLocation me = rc.getLocation();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                // Notice that the Miner's action cooldown is very low.
+                // You can mine multiple times per turn!
+                while (rc.canMineGold(mineLocation)) {
+                    rc.mineGold(mineLocation);
+                }
+                while (rc.canMineLead(mineLocation)&&rc.senseLead(mineLocation)>1) {
+                    rc.mineLead(mineLocation);
+                }
+            }
+        }
+        Direction dir;
+        MapLocation[] k = rc.senseNearbyLocationsWithLead(rc.getType().visionRadiusSquared);
+
+        // hi urav
+        int indexOfBestSquareInK = 0;
+        for(int i = 0; i<k.length;i++)
+        {
+            if(rc.senseLead(k[i])>rc.senseLead(k[indexOfBestSquareInK]))
+                indexOfBestSquareInK=i;
+        }
+        if(k.length == 0)
+            dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        else
+            dir = rc.getLocation().directionTo(k[indexOfBestSquareInK]);
+        if (rc.canMove(dir)) {
+            rc.move(dir);
+            System.out.println("I moved!");
+        }
+    }
+}
