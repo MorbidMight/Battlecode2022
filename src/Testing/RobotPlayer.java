f36d3fda4f31b9d5980af5bb9e85c8691261f255
Seth <sethlifland11@gmail.com>
2022-01-06 20:13:49 -0500

    attacker movement

--- src/Testing/RobotPlayer.java
+++ src/Testing/RobotPlayer.java
@@ -128,7 +128,7 @@ public strictfp class RobotPlayer {
         //add scout implementation
         int radius = rc.getType().actionRadiusSquared;
         Team opponent = rc.getTeam().opponent();
-        RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
+        RobotInfo[] enemies = rc.senseNearbyRobots(rc.getType().actionRadiusSquared, opponent);
         Direction dir = directions[rng.nextInt(directions.length)];
             Team ally = rc.getTeam();
         if (enemies.length > 0){
@@ -149,31 +149,27 @@ public strictfp class RobotPlayer {
             }
         }
         
-        else if (rc.getHealth() < 10) { //low on health, need to run
-            RobotInfo[] allies = rc.senseNearbyRobots(radius, ally);
-            if (allies.length == 0)
-                dir = directions[rng.nextInt(directions.length)];
-            else
-                dir = rc.getLocation().directionTo(allies[0].location);
-            for ( int i = 0; i < allies.length; i ++)
-            {
-                if(allies[i].getType() == RobotType.ARCHON) {
-                    dir = rc.getLocation().directionTo(allies[i].location);
-                    break;
-                }
-            }
-
+           if (rc.getHealth() < 10) { //low on health, need to run
+               RobotInfo[] allies = rc.senseNearbyRobots(radius, ally);
+               if (allies.length == 0)
+                   dir = directions[rng.nextInt(directions.length)];
+               else
+                   dir = rc.getLocation().directionTo(allies[0].location);
+               for (int i = 0; i < allies.length; i++) {
+                   if (allies[i].getType() == RobotType.ARCHON) {
+                       dir = rc.getLocation().directionTo(allies[i].location);
+                       break;
+                   }
+               }
+           }
+            else if (enemies.length > 0)
+                dir = rc.getLocation().directionTo(enemies[a].location);
             if (rc.canMove(dir)) {
                 rc.move(dir);
                 System.out.println("I moved!");
             }
-        }
 
-        else{ 
-            if (rc.canMove(dir)) {
-            rc.move(dir);
-            System.out.println("I moved!");
-        }
-        }
+
+
     }
 }
