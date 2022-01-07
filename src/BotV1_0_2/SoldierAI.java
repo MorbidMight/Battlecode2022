793acb4e7f07dc6f1f885f5bc0fdb701b38aea02
BntM <BinitM@protonmail.com>
2022-01-07 14:12:52 -0500

    BotV1_0_2/ with grouping(unfinished)

--- src/BotV1_0_2/SoldierAI.java
+++ src/BotV1_0_2/SoldierAI.java
@@ -13,6 +13,7 @@ static final double buildpmod = 0.4;
 static final double watchpmod = 0.5;
 static final double sagepmod = 0.7;
 static final double labpmod = 0.5;
+private static Direction dirOfAttack;
 
 static RobotInfo calcprio (RobotInfo[] k) {
     int j = 0; // index holder
@@ -42,24 +43,84 @@ static RobotInfo calcprio (RobotInfo[] k) {
     return k[j];
 }
 static void runSoldier(RobotController rc) throws GameActionException {
+
+     //find and go toward leader soldier
+    Direction dir;
+    boolean isLeader;
+    //make a leader if there is no leader
+    MapLocation myLoc = rc.getLocation();
+    if(rc.readSharedArray(0) == 0){
+        rc.writeSharedArray(0, rc.getID());
+        isLeader = true;
+        //random dir to leader
+        dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        dirOfAttack = dir;
+        //assign ints in the SharedArray to the coordinates of the leader
+        rc.writeSharedArray(1, myLoc.x);
+        rc.writeSharedArray(2, myLoc.y);
+    } 
+    else if(rc.readSharedArray(0) == rc.getID()){
+        dir = dirOfAttack;
+        //change direction if the bot can't move to direction
+        int rotations = 0;
+        while(!rc.canMove(dir) && rotations < 8) {
+            dir = dir.rotateRight();
+            rotations++;
+        }
+        //change leader if leader can't move
+        if(rotations == 8){
+            rc.writeSharedArray(0, 0);
+        }
+        //assign ints in the SharedArray to the coordinates of the leader
+        rc.writeSharedArray(1, myLoc.x);
+        rc.writeSharedArray(2, myLoc.y);
+        //change leader if leader is low in health
+        if(rc.getHealth() < 20){
+            rc.writeSharedArray(0, 0);
+        }
+    }
+    //find the direction of leader and set dir to diection of leader
+    else{
+        if(myLoc.x < rc.readSharedArray(1)){
+            if(myLoc.y < rc.readSharedArray(2)){
+                dir = Direction.NORTHWEST; 
+            }
+            else if(myLoc.y > rc.readSharedArray(2)){
+                dir = Direction.SOUTHWEST;
+            }
+            else{
+                dir = Direction.WEST;
+            }
+        }
+        else{
+            if(myLoc.y < rc.readSharedArray(2)){
+                dir = Direction.NORTHEAST;
+            }
+            else if(myLoc.y > rc.readSharedArray(2)){
+                dir = Direction.SOUTHEAST;
+            }
+            else{
+                dir = Direction.EAST;
+            }
+        }
+    }
     
     // Try to attack someone
     //add scout implementation
     int radius = rc.getType().actionRadiusSquared;
     Team opponent = rc.getTeam().opponent();
     RobotInfo[] enemies = rc.senseNearbyRobots(radius, opponent);
-    Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
     Team ally = rc.getTeam();
-        MapLocation toAttack;
-        if( enemies.length > 0) {
-            toAttack = calcprio(enemies).location; // find who to attack
+    MapLocation toAttack;
+    if( enemies.length > 0) {
+        toAttack = calcprio(enemies).location; // find who to attack
         if (rc.canAttack(toAttack)) {
             rc.attack(toAttack);
         }
         
     }
 
-        if (rc.getHealth() < 10) { //low on health, need to run
+    if (rc.getHealth() < 10) { //low on health, need to run
         RobotInfo[] allies = rc.senseNearbyRobots(radius, ally);
         if (allies.length == 0)
             dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
@@ -79,16 +140,17 @@ static void runSoldier(RobotController rc) throws GameActionException {
         }
     }
 
-        else if (rc.readSharedArray(10) != 0) {
-            MapLocation i = new MapLocation(rc.readSharedArray(10), rc.readSharedArray(11));
-            dir = rc.getLocation().directionTo(i);
-        }
-        else if (enemies.length > 0)
-        dir = rc.getLocation().directionTo(calcprio(enemies).location);
-        if (rc.canMove(dir)) {
-            rc.move(dir);
-            System.out.println("I moved!");
-        }
+   /* else if (rc.readSharedArray(10) != 0) {
+        MapLocation i = new MapLocation(rc.readSharedArray(10), rc.readSharedArray(11));
+        dir = rc.getLocation().directionTo(i);
+    }*/
+    while(!rc.canMove(dir)){
+        dir = dir.rotateLeft();
+    }
+    if (rc.canMove(dir)) {
+        rc.move(dir);
+        System.out.println("I moved!");
+    }
 
 }
 }
