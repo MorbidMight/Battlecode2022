2c53b25fc74462add10ce6891652c2a63e5fdf95
BntM <BinitM@protonmail.com>
2022-01-08 16:02:44 -0500

    added improved soldier to BotV1_1_1

---
+++ src/BotV1_1_1/BuilderAI.java
@@ -0,0 +1,86 @@
+package BotV1_1_1;
+
+import battlecode.common.*;
+
+import java.util.Random;
+
+public class BuilderAI {
+    static final Random rng = new Random(6147);
+
+    static void runBuilder(RobotController rc) throws GameActionException {
+        //move randomly(change later)
+
+        Direction dir;
+        Team ally = rc.getTeam();
+        RobotInfo[] allies = rc.senseNearbyRobots(20, ally);
+
+        //repair
+        Direction ranDir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+        if(rc.readSharedArray(63) == 1) {
+            if (allies.length < 2) {
+                if (rc.canBuildRobot(RobotType.LABORATORY, ranDir)) {
+                    rc.buildRobot(RobotType.LABORATORY, ranDir);
+                    rc.writeSharedArray(63, 2);
+                    rc.writeSharedArray(61, rc.getLocation().add(ranDir).x);
+                    rc.writeSharedArray(62, rc.getLocation().add(ranDir).y);
+                }
+            }
+            else{
+                if(rc.canMove(ranDir)){
+                    rc.move(ranDir);
+                }
+            }
+
+        }
+
+
+        int minerCount = 0;
+        boolean alreadyWatchtower = false;
+        for (int x = 0; x < allies.length; x++) {
+            if (allies[x].getType().equals(RobotType.MINER)) {
+                minerCount++;
+            }
+            if (allies[x].getType().equals(RobotType.WATCHTOWER)) {
+                alreadyWatchtower = true;
+            }
+        }
+
+        for(int x = 0; x < allies.length; x++)
+        {
+            if(allies[x].getType().equals(RobotType.MINER))
+            {
+                if (rc.canMove(rc.getLocation().directionTo(allies[x].getLocation()))){
+                    rc.move(rc.getLocation().directionTo(allies[x].getLocation()));
+                }
+            }
+        }
+        MapLocation q = new MapLocation(rc.readSharedArray(61), rc.readSharedArray(62));
+        if (minerCount >= 4 && !alreadyWatchtower && !rc.canSenseLocation(q)) {//if there are 4 miners in the area and there isn't already a watchtower
+            for (int x = 0; x < RobotPlayer.directions.length; x++) {
+                dir = RobotPlayer.directions[x];
+                if (rc.canBuildRobot(RobotType.WATCHTOWER, dir)) {//look in all directions and see if you can build a watchtower
+                    rc.buildRobot(RobotType.WATCHTOWER, dir);//build it
+                    if(rc.canRepair(rc.getLocation().add(dir))){
+                        rc.repair(rc.getLocation().add(dir));
+                    }
+                    break;
+                }
+            }
+        }
+        RobotInfo lowestTower = null;
+        for(int x = 0; x < allies.length; x++)
+        {
+            if(allies[x].getType().equals(RobotType.WATCHTOWER) || allies[x].getType().equals((RobotType.LABORATORY)))
+            {
+                lowestTower = allies[x];
+            }
+        }
+        if(!(lowestTower.equals(null)))
+        {
+            if (rc.canRepair(lowestTower.getLocation()))
+            {
+                rc.repair(lowestTower.getLocation());
+            }
+        }
+    }
+}
---
+++ src/BotV1_1_1/MinerAI.java
@@ -0,0 +1,110 @@
+package BotV1_1_1;
+
+import battlecode.common.*;
+
+
+public class MinerAI {
+    static int turnssincemined = 0;
+    static boolean isNearArchon = false;
+    static boolean isNotNextToArchon = true;
+
+    /**
+     * Run a single turn for a Miner.
+     * This code is wrapped inside the infinite loop in run(), so it is called once per turn.
+     */
+    static void runMiner(RobotController rc) throws GameActionException {
+        // Try to mine on squares around us.
+        //Mining
+        turnssincemined++;
+        MapLocation me = rc.getLocation();
+        for (int dx = -1; dx <= 1; dx++) {
+            for (int dy = -1; dy <= 1; dy++) {
+                MapLocation mineLocation = new MapLocation(me.x + dx, me.y + dy);
+                // Notice that the Miner's action cooldown is very low.
+                // You can mine multiple times per turn!
+                while (rc.canMineGold(mineLocation)) {
+                    rc.mineGold(mineLocation);
+                    turnssincemined = 0;
+                }
+                while (rc.canMineLead(mineLocation) && rc.senseLead(mineLocation) > 1) {
+                    rc.mineLead(mineLocation);
+                    turnssincemined = 0;
+                }
+            }
+        }
+
+
+        //movement
+        Team ally = rc.getTeam();
+        Direction dir;
+        MapLocation[] k = rc.senseNearbyLocationsWithLead(rc.getType().visionRadiusSquared);
+        int bestPossibleLead = 0;
+        int indexBest = -1;
+        boolean moveRandomly = true;
+        for (int i = 0; i < k.length; i++) {
+            int effectiveLeadAtI = (int)Math.round((rc.senseLead(k[i]) * (1 + rc.senseRubble(k[i]) / 10.0)));
+            if (effectiveLeadAtI > bestPossibleLead && !rc.isLocationOccupied(k[i])) {
+                indexBest = i;
+                bestPossibleLead = (int) (rc.senseLead(k[indexBest]) * (1 + rc.senseRubble(k[indexBest]) / 10.0));
+                moveRandomly = false;
+            }
+        }
+        //Best possible lead is the amount of lead on the best square, 0 if no lead
+        //IndexBest is the index of the best square in K, -1 if no potential lead
+        MapLocation t =  new MapLocation(0,0);
+        RobotInfo[] z = rc.senseNearbyRobots(1, ally);
+        for(int y = 0; y < z.length; y++) {
+            if (z[y].getType() == RobotType.ARCHON) {
+                t = z[y].getLocation();
+                isNotNextToArchon = false;
+                break;
+            }
+        }
+
+        if (moveRandomly) {
+            if(!isNotNextToArchon){
+                dir = rc.getLocation().directionTo(t).opposite();
+                if(!rc.canMove(dir)){
+                    dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+
+                }
+            }
+
+            dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
+
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+            }
+            //move only if true
+
+        } else if (!k[indexBest].equals(rc.getLocation())&&  rc.senseLead(k[indexBest]) > 10 + rc.senseLead(rc.getLocation()) || rc.senseLead(rc.getLocation()) == 0) {
+            dir = rc.getLocation().directionTo(k[indexBest]);
+            if (rc.canMove(dir)) {
+                rc.move(dir);
+            }
+        }
+
+        RobotInfo[] p = rc.senseNearbyRobots(rc.getType().visionRadiusSquared, ally);
+        RobotInfo[] z2 = rc.senseNearbyRobots(1, ally);
+        for (int i = 0; i < p.length; i++) {
+            if (p[i].getType() == RobotType.ARCHON) {
+                isNearArchon = true;
+                break;
+            }
+        }
+        for(int y = 0; y < z2.length; y++) {
+            if (z2[y].getType() == RobotType.ARCHON) {
+                isNotNextToArchon = false;
+                break;
+            }
+        }
+        if (isNearArchon && turnssincemined > 50 && isNotNextToArchon) {
+            rc.disintegrate();
+        }
+
+        isNearArchon = false;
+
+    }
+}
+
+
