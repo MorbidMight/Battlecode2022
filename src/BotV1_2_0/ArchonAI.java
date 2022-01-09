21f981410ab5ad34a141d03d78d0bfeab892fb6a
Todd <todd.mcknight06@gmail.com>
2022-01-09 17:22:15 -0500

    Did a bunch of shit

--- src/BotV1_2_0/ArchonAI.java
+++ src/BotV1_2_0/ArchonAI.java
@@ -12,53 +12,41 @@ public class ArchonAI {
     static void runArchon(RobotController rc) throws GameActionException {
         // Pick a direction to build in.
         //check for a possible repair
-        Team ally = rc.getTeam();
+
         Direction dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(RobotPlayer.directions.length)];
-        RobotInfo[] k = rc.senseNearbyRobots(20, ally);
-        //for loop to check which possible heal is lowest on health
-        int q = 100; // holder variable for lowest possible health to heal
-        int w = 0; // holder variable for index of lowest possible heal
-        if (k.length >= 1) {
-            for (int a = 0; a < k.length; a++) {
-                if (k[a].getHealth() < q) {
-                    q = k[a].getHealth();
-                    w = a;
-                }
-            }
-            if (q < 15 && rc.canRepair(k[w].getLocation()))
-                rc.repair(k[w].getLocation());
-        }
-        if(RobotPlayer.turnCount > 500 && labCount == 0 && rc.getMapHeight() > 30 && rc.getMapWidth() > 30){
-            if(rc.getTeamLeadAmount(rc.getTeam()) >= 950){
-                if(rc.canBuildRobot(RobotType.BUILDER, rc.getLocation().directionTo(Utilities.findCenter(rc)).opposite())){
-                    rc.buildRobot(RobotType.BUILDER, rc.getLocation().directionTo(Utilities.findCenter(rc)).opposite());
-                    labCount++;
-                    rc.writeSharedArray(63, 1);
-                }
-            }
-        }
-        else if (rc.getTeamLeadAmount(rc.getTeam()) >= 180 && (RobotPlayer.turnCount < 500 || rc.readSharedArray(63) == 2 || rc.getMapWidth() < 30 || rc.getMapHeight() < 30)) {
-            double randDouble = Math.random();
-            if (randDouble > 0.65) {
-                // Let's try to build a miner.
-                rc.setIndicatorString("Trying to build a miner");
-                dir = RobotPlayer.cardinalDirections[RobotPlayer.rng.nextInt(4)];
-                if (rc.canBuildRobot(RobotType.MINER, dir)) {
-                    rc.buildRobot(RobotType.MINER, dir);
 
-                }
-            } else if (randDouble > 0.1 && randDouble < 0.65) {
-                // Let's try to build a soldier.
-                rc.setIndicatorString("Trying to build a soldier");
-                if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
-                    rc.buildRobot(RobotType.SOLDIER, dir);
 
+        if (RobotPlayer.turnCount < 500 && rc.readSharedArray(RobotPlayer.ArchonID) == rc.getID() || RobotPlayer.turnCount > 500) {
+            if (RobotPlayer.turnCount > 500 && labCount == 0 && rc.getMapHeight() > 30 && rc.getMapWidth() > 30) {
+                if (rc.getTeamLeadAmount(rc.getTeam()) >= 950) {
+                    if (rc.canBuildRobot(RobotType.BUILDER, rc.getLocation().directionTo(Utilities.findCenter(rc)).opposite())) {
+                        rc.buildRobot(RobotType.BUILDER, rc.getLocation().directionTo(Utilities.findCenter(rc)).opposite());
+                        labCount++;
+                        rc.writeSharedArray(63, 1);
+                    }
                 }
-            }
-            else if (randDouble < 0.05) {//ensure there aren't more than one builder per archon
-                rc.setIndicatorString("Trying to build a builder");
-                if (rc.canBuildRobot(RobotType.BUILDER, dir)) {
-                    rc.buildRobot(RobotType.BUILDER, dir);
+            } else if (rc.getTeamLeadAmount(rc.getTeam()) >= 180 && (RobotPlayer.turnCount < 500 || rc.readSharedArray(63) == 2 || rc.getMapWidth() < 30 || rc.getMapHeight() < 30)) {
+                double randDouble = Math.random();
+                if (randDouble > 0.65) {
+                    // Let's try to build a miner.
+                    dir = RobotPlayer.directions[RobotPlayer.rng.nextInt(4) * 2];
+                    rc.setIndicatorString("Trying to build a miner");
+                    if (rc.canBuildRobot(RobotType.MINER, dir)) {
+                        rc.buildRobot(RobotType.MINER, dir);
+
+                    }
+                } else if (randDouble > 0.1 && randDouble < 0.65) {
+                    // Let's try to build a soldier.
+                    rc.setIndicatorString("Trying to build a soldier");
+                    if (rc.canBuildRobot(RobotType.SOLDIER, dir)) {
+                        rc.buildRobot(RobotType.SOLDIER, dir);
+
+                    }
+                } else if (randDouble < 0.05) {//ensure there aren't more than one builder per archon
+                    rc.setIndicatorString("Trying to build a builder");
+                    if (rc.canBuildRobot(RobotType.BUILDER, dir)) {
+                        rc.buildRobot(RobotType.BUILDER, dir);
+                    }
                 }
             }
         }
@@ -93,6 +81,33 @@ public class ArchonAI {
                     if (AX4 == 0 && AY4 == 0) {
                         rc.writeSharedArray(57, rc.getLocation().x);
                         rc.writeSharedArray(58, rc.getLocation().y);
+                    }
+                }
+            }
+        }
+    }
+    public static void writeIDToArray (RobotController rc) throws GameActionException {
+        int ID1 = rc.readSharedArray(44);
+        if (ID1 == 0) {
+            rc.writeSharedArray(44, rc.getID());
+        } else if (rc.getID() == ID1) {
+            return;
+        } else {
+            int ID2 = rc.readSharedArray(45);
+            if (ID2 == 0) {
+                rc.writeSharedArray(45, rc.getID());
+            } else if (rc.getID() == ID2) {
+                return;
+            } else {
+                int ID3 = rc.readSharedArray(46);
+                if (ID3 == 0) {
+                    rc.writeSharedArray(46, rc.getID());
+                } else if (rc.getID() == ID3) {
+                    return;
+                } else {
+                    int ID4 = rc.readSharedArray(47);
+                    if (ID4 == 0) {
+                        rc.writeSharedArray(47, rc.getID());
                     }
                 }
             }
@@ -138,4 +153,3 @@ public class ArchonAI {
         return locs;
     }
 }
-
