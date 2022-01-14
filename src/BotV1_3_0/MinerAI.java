83985573dcaa99079c931861c0dd68cd6023c33f
Todd <todd.mcknight06@gmail.com>
2022-01-13 21:32:44 -0500

    Miners consider rubbble in thier pathfinding
    changed the seed of RobotPlayer.rng

--- src/BotV1_3_0/MinerAI.java
+++ src/BotV1_3_0/MinerAI.java
@@ -5,7 +5,6 @@ import battlecode.common.*;
 
 public class MinerAI {
     static int turnssincemined = 0;
-    static int whenLostGo = RobotPlayer.rng.nextInt(4);
     static int turnsSinceMoved = 0;
     static Direction dGoing = RobotPlayer.directions[RobotPlayer.rng.nextInt(8)];
     ;
@@ -47,56 +46,54 @@ public class MinerAI {
                 break;
             }
         }
-        if (rc.senseLead(rc.getLocation())<2) {
-            RobotInfo[] AllFriendlyRobots = rc.senseNearbyRobots(rc.getType().visionRadiusSquared, rc.getTeam());
-            for (RobotInfo hh : AllFriendlyRobots) {
-                if (hh.getType().equals(RobotType.MINER)) {
-                    Direction dGoing = rc.getLocation().directionTo(hh.getLocation()).opposite();
-                    if (!tryToMove(rc, dGoing)) {
-                        //rc.disintegrate();
-                    }
-                    break;
+
+        RobotInfo[] AllFriendlyRobots = rc.senseNearbyRobots(rc.getType().visionRadiusSquared, rc.getTeam());
+        for (RobotInfo hh : AllFriendlyRobots) {
+            if (hh.getType().equals(RobotType.MINER)) {
+                Direction dGoing = rc.getLocation().directionTo(hh.getLocation()).opposite();
+                if (!tryToMove(rc, dGoing)) {
+                    //rc.disintegrate();
                 }
+                break;
             }
+        }
 
 
 //find the best lead
-            Direction dir;
-            MapLocation[] k = rc.senseNearbyLocationsWithLead(rc.getType().visionRadiusSquared);
-            int bestPossibleLead = 0;
-            int indexBest = 0;
-            boolean moveRandomly = true;
-            for (int i = 0; i < k.length; i++) {
-                int effectiveLeadAtI = (int) Math.round((rc.senseLead(k[i]) * (1 + rc.senseRubble(k[i]) / 10.0)));
-                if (effectiveLeadAtI > bestPossibleLead && !rc.isLocationOccupied(k[i])) {
-                    indexBest = i;
-                    bestPossibleLead = (int) (rc.senseLead(k[indexBest]) * (1 + rc.senseRubble(k[indexBest]) / 10.0));
-                    moveRandomly = false;
-                }
+        Direction dir;
+        MapLocation[] k = rc.senseNearbyLocationsWithLead(rc.getType().visionRadiusSquared);
+        int bestPossibleLead = 0;
+        int indexBest = 0;
+        boolean moveRandomly = true;
+        for (int i = 0; i < k.length; i++) {
+            int effectiveLeadAtI = (int) Math.round((rc.senseLead(k[i]) * (1 + rc.senseRubble(k[i]) / 10.0)));
+            if (effectiveLeadAtI > bestPossibleLead && !rc.isLocationOccupied(k[i])) {
+                indexBest = i;
+                bestPossibleLead = (int) (rc.senseLead(k[indexBest]) * (1 + rc.senseRubble(k[indexBest]) / 10.0));
+                moveRandomly = false;
             }
-            //Best possible lead is the amount of lead on the best square, 0 if no lead
-            //IndexBest is the index of the best square in K, -1 if no potential lead
+        }
+        //Best possible lead is the amount of lead on the best square, 0 if no lead
+        //IndexBest is the index of the best square in K, -1 if no potential lead
 
-            if (!moveRandomly) {
-                dGoing = rc.getLocation().directionTo(k[indexBest]);
-                tryToMove(rc, dGoing);
-            } else {
-                tryToMove(rc, dGoing);
-            }
+        if (!moveRandomly&&rc.senseLead(k[indexBest])>1) {
+            dGoing = rc.getLocation().directionTo(k[indexBest]);
+            tryToMove(rc, dGoing);
 
-
+        } else {
+            tryToMove(rc, dGoing);
         }
     }
 
     static boolean tryToMove(RobotController rc, Direction d) throws GameActionException {
-        if (rc.canMove(d)) {
+        if (rc.canMove(d) && rc.senseRubble(rc.getLocation().add(d)) < 50) {
             rc.move(d);
             return true;
-        } else if (rc.canMove(d.rotateRight())) {
+        } else if (rc.canMove(d) && rc.senseRubble(rc.getLocation().add(d.rotateRight())) < 50) {
             d = d.rotateRight();
             rc.move(d.rotateRight());
             return true;
-        } else if (rc.canMove(d.rotateLeft())) {
+        } else if (rc.canMove(d) && rc.senseRubble(rc.getLocation().add(d.rotateLeft())) < 50) {
             d = d.rotateLeft();
             rc.move(dGoing.rotateLeft());
             return true;
